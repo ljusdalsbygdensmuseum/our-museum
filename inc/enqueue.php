7508d0043ed3b16eb5a66b6aa984da5a004cd25a
Ina Jobbet <ina.eklund@ljm.se>
2026-04-20 09:20:49 +0200

    fixed a typo

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -8,10 +8,10 @@ function OMUS_enqueue($hook)
         $assets = include(get_template_directory() . '/build/frontend.asset.php');
 
         //Enqueue scripts
-        wp_enqueue_script('omos-frontend-script', get_theme_file_uri('/build/frontend.js'), $assets['dependencies'], $assets['version'], true);
+        wp_enqueue_script('omus-frontend-script', get_theme_file_uri('/build/frontend.js'), $assets['dependencies'], $assets['version'], true);
 
         //Enqueue styles
-        wp_enqueue_style('omos-frontend-style', get_theme_file_uri('/build/frontend.css'));
+        wp_enqueue_style('omus-frontend-style', get_theme_file_uri('/build/frontend.css'));
 
         //Set translation
         //wp_set_script_translations('mptab-exhibitions', 'mptab-domain', plugin_dir_path(__FILE__) . '/languages');
